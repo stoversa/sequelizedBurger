fde7a11ba2211ce62e86addc42299257e80d48ef
stoversa <stoversa@gmail.com>
2018-03-25 18:50:54 -0400

    Updated for Sequelize

--- server.js
+++ server.js
@@ -1,26 +1,33 @@
+//Dependencies
 const express = require("express");
 const bodyParser = require("body-parser");
-//connection for Heroku
-const PORT = process.env.PORT || 8080;
-
-//express
-const app = express();
-app.use(express.static("public"));
-
-//body parser
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-//handlebars
 const exphbs = require("express-handlebars");
+var db = require("./models");
+
+//Create Express App
+const app = express();
+
+//Handlebars configuration
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-//controller
-const router = require("./controllers/burgersController.js");
-app.use(router);
+//Body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+//Static Directory
+app.use(express.static("public"));
+
+//Controllers
+require("./controllers/api")(app);
+require("./controllers/html")(app);
+
+//ports and connection for Heroku
+const PORT = process.env.PORT || process.argv[2] || 8080;
 //listener
-app.listen(PORT, () => {
-    console.log(`Server listening on: http://localhost:${PORT}`);
+
+db.sequelize.sync().then(function(){
+    app.listen(PORT, () => {
+        console.log(`Server listening on: http://localhost:${PORT}`);
+    });
 });
